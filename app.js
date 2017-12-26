0755174483915630d949eb61b7f2bf176fa6871a
Eugine1988 <evguenimetlitski@hotmail.com>
2017-12-26 19:34:53 +1000

    Autherntication added

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Body Parser Middleware
 app.use(bodyParser.json());
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 // Anything with '/users/anything' in browser will go to users route
 app.use('/users', users);
 
